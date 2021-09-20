1feb12895c3d67b0ab944ffa7fe18077e500bcfd
curso14 curso14 <curso14@noexiste.no>
2021-09-20 11:06:45 +0200

    Commit inicial

---
+++ src/main/java/es/cic/bootcamp/grupo02final/Grupo02FinalApplication.java
@@ -0,0 +1,13 @@
+package es.cic.bootcamp.grupo02final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo02FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo02FinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/cic/bootcamp/grupo02final/Grupo02FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cic.bootcamp.grupo02final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo02FinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
